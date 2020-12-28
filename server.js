a6057d13d559fd826ae17a888aa26491de29524d
prats7 <pratyushvishal04@gmail.com>
2020-12-28 14:22:35 +0530

    Update client

--- server.js
+++ server.js
@@ -17,6 +17,6 @@ app.use('/api/tasks',tasks);
 
 
 //Express server
-const PORT = process.env.PORT || 5000 ;
+const PORT = 5000 || process.env.PORT ;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
