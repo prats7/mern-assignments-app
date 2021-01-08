676081fd259245c1bcfc9cc46e52d9abece4b427
prats7 <pratyushvishal04@gmail.com>
2021-01-08 17:49:27 +0530

    Version 1.0.0

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 
 
@@ -17,7 +18,17 @@ app.use('/api/user',require('./routes/api/user'));
 app.use('/api/auth',require('./routes/api/auth'));
 
 
+//Serve static assets if in production
+if(process.env.NODE_ENV === 'production'){
+    //Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    });
+}
+
 //Express server
-const PORT = 5000 || process.env.PORT ;
+const PORT = process.env.PORT || 5000 ;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
